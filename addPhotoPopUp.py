3c3cd3850331ba2c91c241c8b84069ae0ea40f1e
Sdanielgg <40220220@esmad.ipp.pt>
2023-12-30 22:22:12 +0000

    photo pop up e small changes to album

--- addPhotoPopUp.py
+++ addPhotoPopUp.py
@@ -1,6 +1,35 @@
 from tkinter import *
+from tkinter import filedialog
+from PIL import Image,ImageTk
 
+#Functions
+
+def open_image():
+    file_path = filedialog.askopenfilename(title="Select Image File", filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
+    if file_path:
+        display_image(file_path)
+
+
+def display_image(file_path):
+    # Open the image
+    original_image = Image.open(file_path)
+
+    aspect_ratio = original_image.width / original_image.height
+    
+    target_size = (200, int(200 / aspect_ratio))
+    resized_image = original_image.resize(target_size, Image.ANTIALIAS)
+    
+    photo = ImageTk.PhotoImage(resized_image)
+    
+    photoCanvas.config(width=target_size[0], height=target_size[1])
+    
+    photoCanvas.create_image(0, 0, anchor=NW, image=photo)
+    photoCanvas.image = photo
+
+
+#Window
 photoPopUp=Tk()
+photoPopUp.configure(bg="grey")
 
 screenWidth = photoPopUp.winfo_screenwidth()
 screenHeight = photoPopUp.winfo_screenheight()
@@ -13,19 +42,43 @@ y = (screenHeight/2) - (appHeight/2)
 
 photoPopUp.geometry(f"{appWidth}x{appHeight}+{int(x)}+{int(y)}")
 
+#Containers
+backCon=Frame(photoPopUp,bg="grey")
+backCon.pack(side=TOP)
+container1=Frame(photoPopUp,bg="grey")
+container1.pack(side=TOP)
+
+container2=Frame(photoPopUp,bg="grey")
+container2.pack()
+
+photoContainer=Frame(photoPopUp)
+photoContainer.pack()
+
+
+#Go back Button
+
+goBack=Button(backCon,text="Go Back",width=20,font=11)
+goBack.pack(side=LEFT)
 
 #photo path button
-photoPath=Button(photoPopUp,text="Photo Path",width=30)
-photoPath.place(x=0,y=0)
+
+photoPath=Button(container2,text="Photo Path",width=30,font=11,command=open_image)
+photoPath.pack(side=TOP)
+
+photoCanvas=Canvas(container2,bg="grey",width=200,height=200)
+photoCanvas.pack(side=TOP)
 
 #photo description (only a few char)
 
-descriptionLabel=Label(photoPopUp,text="Description",font=11)
-descriptionLabel.place(x=30,y=0)
+descriptionLabel=Label(container2,text="Description",font=11,bg="grey")
+descriptionLabel.pack(side=TOP)
 
-description=Text(photoPopUp,height=5,width=40,font=11)
-description.place(x=30,y=30)
+description=Text(container2,height=5,width=40,border=10,font=11)
+description.pack(side=BOTTOM)
+
+#Add photo to album
 
 addPhotoButton=Button(photoPopUp,text="Add Photo",font=11)
+addPhotoButton.pack(side=RIGHT)
  
 photoPopUp.mainloop()
--- album.py
+++ album.py
@@ -2,13 +2,13 @@ from tkinter import *
 from tkinter import ttk
 
 album=Tk()  
-album.geometry("1000x600")
 album.resizable(0,0)
+album.configure(bg="#D9D9D9")
 screenWidth = album.winfo_screenwidth()
 screenHeight = album.winfo_screenheight()
 
 appWidth =1000
-appHeight = 600
+appHeight = 600 
 
 x = (screenWidth/2) - (appWidth/2)
 y = (screenHeight/2) - (appHeight/2)
@@ -19,20 +19,33 @@ album.geometry(f"{appWidth}x{appHeight}+{int(x)}+{int(y)}")
 title=Label(album,text="selectedAlbum",font=11)
 title.place(x=0,y=0)
 
-removeButton=Button(album,text="Remove Photo",font=11)
-removeButton.place(x=400,y=100)
+removeButton=Button(album,text="Remove Photo",font=11,width=20,height=2,bg="#D9D9D9")
+removeButton.place(x=110,y=220)
 
-addButton=Button(album,text="Add Photo",font=11)
-addButton.place()
+addButton=Button(album,text="Add Photo",font=11,width=20,height=2,bg="#D9D9D9")
+addButton.place(x=110,y=320)
+openButton=Button(album,text="Open Photo",font=11,width=20,height=2,bg="#D9D9D9")
+openButton.place(x=110,y=420)
 
-tree = ttk.Treeview(album, selectmode="browse", columns=("Name","Description"), show="headings", height=10)
+#Tree view de albums
+
+tree = ttk.Treeview(album, selectmode="browse", columns=("Name","Description","FileType"), show="headings", height=20)
+
+style = ttk.Style()
+style.configure("Treeview", highlightthickness=0, bd=0, font=('Calibri', 11)) 
+style.configure("Treeview.Heading", font=('Comic Sans MS', 13)) 
+style.layout("Treeview", [('mystyle.Treeview.treearea', {'sticky': 'nswe'})]) 
+
 tree.column("Name", width=140,anchor="center")
 tree.heading("Name",text="Name")
 
-tree.column("Description", width=200,anchor="center")
+tree.column("Description", width=250,anchor="center")
 tree.heading("Description",text="Description")
-tree.column
+tree.column("FileType", width=140,anchor="center")
+tree.heading("FileType",text="FileType")
 tree.place(x=400,y=100)
+
+
 
 
 album.mainloop()
