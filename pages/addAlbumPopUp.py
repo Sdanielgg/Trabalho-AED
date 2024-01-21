b05ecdd159e7b9d14c7e24649133126b33b95b6c
Sdanielgg <40220220@esmad.ipp.pt>
2024-01-21 22:04:13 +0000

    add Album funcional!

---
+++ pages/addAlbumPopUp.py
@@ -0,0 +1,90 @@
+from tkinter import *
+from tkinter import messagebox
+
+# Functions
+def load_users():
+    f=open("files\\users.txt","r",encoding="utf-8")
+    lines=f.readlines()
+    f.close()
+    for line in lines:
+        content=line.strip().split(";")
+        if (content[3]=="Logged"):
+            user=content[0]
+        return user
+
+def load_categories():
+    categories = []
+    f_categories = open("files\\categories.txt", "r", encoding="utf-8")
+    lines = f_categories.readlines()
+    f_categories.close()
+    for line in lines:
+        content = line.strip().split(";")
+        categories.append(content[0])
+    f_categories.close()
+    return categories
+
+def addAlbum():
+    user=load_users()
+    AlbumName=txt_AlbumName.get()
+    Description=description.get()
+    Category=value_inside.get()
+    f = open("files\\AlbumList.txt", "r", encoding="utf-8")
+    lines = f.readlines()
+    for line in lines:
+        content=line.strip().split(";")
+        if (content[1]==AlbumName):
+            messagebox.showerror(title="Album Name in use",message="That Album Name is already in use, please choose another one!")
+            return
+        
+    f.close()
+    newLine =user+";"+ AlbumName+";"+Category+";"+Description+";"+ "\n"
+    f = open("files\\AlbumList.txt", "a", encoding="utf-8")
+    f.write(newLine)
+    f.close()
+
+# Window
+AlbumPopUp = Tk()
+AlbumPopUp.configure(bg="grey")
+
+screenWidth = AlbumPopUp.winfo_screenwidth()
+screenHeight = AlbumPopUp.winfo_screenheight()
+
+appWidth = 1000
+appHeight = 500
+
+x = (screenWidth/2) - (appWidth/2)
+y = (screenHeight/2) - (appHeight/2)
+
+AlbumPopUp.geometry(f"{appWidth}x{appHeight}+{int(x)}+{int(y)}")
+
+# Go back Button
+go_Back = Button(text="Go Back", width=15, font=11)
+go_Back.place(x=10,y=10)
+
+# Album Name
+label_AlbumName = Label(text="Album Name:", font=14, bg="grey")
+label_AlbumName.place(x=30,y=120)
+
+txt_AlbumName = Entry(width=19, font=14)
+txt_AlbumName.place(x=155,y=120)
+
+# Categories
+categories = load_categories()
+value_inside = StringVar()
+value_inside.set("Select a Category")
+
+genreSelect = OptionMenu(AlbumPopUp, value_inside, *categories)
+genreSelect.place(x=40,y=200)
+
+# Album Description
+descriptionLabel = Label(text="Description",font=14, bg="grey")
+descriptionLabel.place(x=500,y=80)
+
+description = Entry( width=40, border=10, font=11)
+description.place(x=500,y=120)
+
+#
+addPhotoButton = Button(AlbumPopUp, text="Create Album", font=11,command=addAlbum)
+addPhotoButton.place(x=820,y=410)
+
+AlbumPopUp.mainloop()
--- pages/userAlbums.py
+++ pages/userAlbums.py
@@ -1,32 +1,50 @@
 from tkinter import *
 from tkinter import ttk
 
-userAlbums=Tk()
+album=Tk()  
+album.resizable(0,0)
+album.configure(bg="#D9D9D9")
+screenWidth = album.winfo_screenwidth()
+screenHeight = album.winfo_screenheight()
 
-screenWidth = userAlbums.winfo_screenwidth()
-screenHeight = userAlbums.winfo_screenheight()
-
-appWidth = 1000
-appHeight = 600
+appWidth =1000
+appHeight = 600 
 
 x = (screenWidth/2) - (appWidth/2)
 y = (screenHeight/2) - (appHeight/2)
 
-userAlbums.geometry(f"{appWidth}x{appHeight}+{int(x)}+{int(y)}")
-userAlbums.resizable(0,0)
+album.geometry(f"{appWidth}x{appHeight}+{int(x)}+{int(y)}")
 
-#tree view and buttons 
-deleteButton=Button()
-addButton=Button()
-myAlbums=ttk.Treeview(selectmode="browse",columns=("Name","Description","Category"),show="headings",height=10)
-myAlbums.column("Description",width=140,anchor="center")
-myAlbums.heading("Description",text="Description")
+#Page Title
+title=Label(album,text="Make a button here to go back to home",font=11,bg="#D9D9D9")
+title.place(x=0,y=0)
 
-myAlbums.column("Category",width=140,anchor="center")
-myAlbums.heading("Category",text="Category")
+removeButton=Button(album,text="Remove Album",font=11,width=20,height=2,bg="#D9D9D9")
+removeButton.place(x=110,y=220)
 
-myAlbums.column("Name", width=140,anchor="center")
-myAlbums.heading("Name",text="Name")
+addButton=Button(album,text="Add Album",font=11,width=20,height=2,bg="#D9D9D9")
+addButton.place(x=110,y=320)
+openButton=Button(album,text="Open Album",font=11,width=20,height=2,bg="#D9D9D9")
+openButton.place(x=110,y=420)
+
+#Tree view de albums
+
+tree = ttk.Treeview(album, selectmode="browse", columns=("Name","Description","FileType"), show="headings", height=20)
+
+style = ttk.Style()
+style.configure("Treeview", highlightthickness=0, bd=0, font=('Calibri', 11)) 
+style.configure("Treeview.Heading", font=('Comic Sans MS', 13)) 
+style.layout("Treeview", [('mystyle.Treeview.treearea', {'sticky': 'nswe'})]) 
+
+tree.column("Name", width=140,anchor="center")
+tree.heading("Name",text="Name")
+tree.column("Description", width=250,anchor="center")
+tree.heading("Description",text="Description")
+tree.column("FileType", width=140,anchor="center")
+tree.heading("FileType",text="FileType")
+tree.place(x=400,y=100)
 
 
-userAlbums.mainloop()
+
+
+album.mainloop()
