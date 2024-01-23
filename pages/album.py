210dd2be07681e8b0728f6c33efe555753c2e647
Sdanielgg <40220220@esmad.ipp.pt>
2024-01-23 23:23:43 +0000

    comments, missing some stuff

--- pages/album.py
+++ pages/album.py
@@ -9,16 +9,31 @@ class Album(Frame):
         self.master = master
         self.create_widgets()
         self.load_album()
+        self.load_user()
 
-    def load_album(self):
+    def load_user(self):
+        f=open("files\\users.txt","r",encoding="utf-8")
+        lines=f.readlines()
+        f.close()
+        for line in lines:
+            content=line.split(";")
+            if  (content[3]=="Logged"):
+                user=content[0]
+                return user
+        
+    def load_albumName(self):
         f=open("files\\Album.txt","r",encoding="utf-8")
         lines=f.readlines()
         albumname=lines[0]
         f.close()
+        return albumname
+
+    def load_album(self):
+        albumname=self.load_albumName()
         f=open("files\\AlbumList.txt","r",encoding="utf-8")
         lines=f.readlines()
         f.close
-        self.page_title.config(text="Album:"+albumname)
+        self.page_title.config(text="Album Name: "+albumname)
         for line in lines:
             content = line.strip().split(";")
             if content[0] == albumname:
@@ -31,7 +46,6 @@ class Album(Frame):
                 for i in range(num_elements):
                     self.tree.insert('', 'end', values=(content[5 + i]))
                     
-
     def line_len(self):
         f=open("files\\AlbumList.txt","r",encoding="utf-8")
         lines=f.readlines()
@@ -45,7 +59,6 @@ class Album(Frame):
         home.main()
     #window
     def display_image(self):
-        selected = self.tree.selection()[0]
         currentItem = self.tree.focus()
         file_path = self.tree.item(currentItem, "values")[0]
         original_image = Image.open(file_path)
@@ -60,10 +73,35 @@ class Album(Frame):
         
         self.photoCanvas.create_image(0, 0, anchor=NW, image=photo)
         self.photoCanvas.image = photo
+    def comment(self):
+        f=open("files\\Album.txt","r",encoding="utf-8")
+        lines=f.readlines()
+        albumname=lines[0]
+        f.close()
+        text=self.commentEntry.get()
+        user=self.load_album()
+        f=open("files\\comments.txt","w",encoding="utf-8")
+        line=str(albumname)+";"+str(user)+";"+str(text)+";\n"
+        f.write(line)
+        f.close()
+        self.loadcomments()
+
+    def loadcomments(self):
+        albumname=self.load_albumName()
+        f=open("files\\comments.txt","r",encoding="utf-8")
+        lines=f.readlines()
+        f.close()
+        for line in lines:
+            content=line.strip().split(";")
+            if content[0]==albumname:
+                self.comments.insert('', 'end', values=(content[1],content[2]))
+
+
+        
     def create_widgets(self):
 
         #Page Title
-        self.page_title=Label(self,text="Album title",font=20)
+        self.page_title=Label(self,text="",font=20)
         self.page_title.place(x=500,y=10)
 
         tree_title=Label(self,text="Photos",font=14)
@@ -92,11 +130,30 @@ class Album(Frame):
 
         self.tree.column("PhotoName", width=300,anchor="center")
         self.tree.heading("PhotoName",text="Photo Name")
-        self.tree.place(x=500,y=100)
+        self.tree.place(x=600,y=100)
+
+        #comments Label/Entry/Button
+        commentLabel=Label(self,text="Comment:",font=7)
+        commentLabel.place(x=10,y=650)
+        self.commentEntry=Entry(self, width=25,font=11)
+        self.commentEntry.place(x=110,y=650)
+        self.button=Button(self,text="Comment",width=10,command=self.comment)
+        self.button.place(x=350,y=650)
+
+        #comments
+        self.comments = ttk.Treeview(self, selectmode="browse", columns=("User","Comment"), show="headings", height=10)
+
+        self.comments.column("User", width=150,anchor="center")
+        self.comments.heading("User",text="User")
+        self.comments.column("Comment", width=300)
+        self.comments.heading("Comment",text="Comment")
+        self.comments.place(x=10,y=400)
 
         #
         self.photoCanvas=Canvas(self,width=200,height=200,bg="gray")
-        self.photoCanvas.place(x=900,y=200)
+        self.photoCanvas.place(x=950,y=200)
+        
+
 def main():
     album = Tk()
     album.configure(bg="#27544C")
@@ -112,4 +169,4 @@ def main():
     user_album_page = Album(master=album)
     user_album_page.pack(expand=True, fill="both")
     album.mainloop()
-
+main()
